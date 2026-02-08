3c62e2175435690a4f78a4fb0f49072690024e87
Dane <dane.jackson@madmobile.com>
2026-02-08 17:54:31 -0500

    First commit

---
+++ app/Actions/Fortify/CreateNewUser.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Actions\Fortify;
+
+use App\Concerns\PasswordValidationRules;
+use App\Concerns\ProfileValidationRules;
+use App\Models\User;
+use Illuminate\Support\Facades\Validator;
+use Laravel\Fortify\Contracts\CreatesNewUsers;
+use Illuminate\Support\Facades\Hash;
+
+class CreateNewUser implements CreatesNewUsers
+{
+    use PasswordValidationRules, ProfileValidationRules;
+
+    /**
+     * Validate and create a newly registered user.
+     *
+     * @param  array<string, string>  $input
+     */
+    public function create(array $input): User
+    {
+        Validator::make($input, [
+            ...$this->profileRules(),
+            'password' => $this->passwordRules(),
+        ])->validate();
+
+        return User::create([
+            'name' => $input['name'],
+            'email' => $input['email'],
+            'confirmed' => false,
+            'validated_at' => null,
+            'confirmed_at' => null,
+            'email_verified_at' => null, 
+            'password' => Hash::make($input['password']),
+        ]);
+    }
+}
